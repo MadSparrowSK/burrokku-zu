939e41d857c4c55661ce6aab5cfc1924fe79357b
MadSparrow <theflash02@list.ru>
2021-02-21 10:45:30 +0300

    Interface 1.0

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interface_1._0
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region DragMainWindow
+        private void Mouse_Drag_Window(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        #endregion
+
+        #region Interactions with Header
+        private void Mouse_Enter(object sender, RoutedEventArgs e)
+        {
+            ((Label)sender).Background = Brushes.Gray;
+        }
+        private void Mouse_Leave(object sender, RoutedEventArgs e)
+        {
+            ((Label)sender).Background = Brushes.Black;
+        }
+        private void Close_Click(object sender, RoutedEventArgs e) 
+        {
+            this.Close();
+        }
+        #endregion
+
+        #region Interactions with SideBar
+        private void SideBar_Mouse_Enter(object sender, RoutedEventArgs e)
+        {
+            ((Label)sender).Background = Brushes.Gray;
+        }
+        private void SideBar_Mouse_Leave(Object sender, RoutedEventArgs e)
+        {
+            ((Label)sender).Background = Brushes.Black;
+        }
+        #endregion
+
+        #region Show Window with Settings
+        private void Change_Window(object sender, RoutedEventArgs e)
+        {
+            Window1 CW = new Window1();
+            CW.Show();
+        }
+        #endregion
+
+        #region ResizeMainWindow
+        private void Resize_Wondow(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else
+                this.WindowState = WindowState.Maximized;
+        }
+        private void Roll_Up(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        #endregion
+    }
+}
