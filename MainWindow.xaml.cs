714bd62d931126375a5ff7b0ccbcc764d2ceba73
MadSparrow <theflash02@list.ru>
2021-03-01 21:39:08 +0300

    зявс

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -507,5 +507,26 @@ namespace Interface_1._0
             CanvasPos.Children.Clear();
         }
         #endregion
+
+        #region infinite_area
+
+        #endregion
+    }
+
+    public class ScrollLimitConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is double && values[1] is double)
+            {
+                return (double)values[0] == (double)values[1];
+            }
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
