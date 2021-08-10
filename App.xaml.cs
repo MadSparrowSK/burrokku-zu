0ca30ccbcee2fbabe047a11f19ef9309caec1e0a
BayHock <79415305+BayHock@users.noreply.github.com>
2021-08-10 15:50:08 +0300

    Так значит я залил первый раз наконец то
    Справка от Олега

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Interface_1._0
+namespace Spravka
 {
     /// <summary>
     /// Логика взаимодействия для App.xaml
